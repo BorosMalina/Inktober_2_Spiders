a2b3894b1bc193fb3b44631a646981079483f65d
Malina <borosmalina@gmail.com>
2023-10-05 21:11:43 +0300

    changed spider movement

--- sketch.js
+++ sketch.js
@@ -1,37 +1,30 @@
 let spiderSize;
 
-let posX = 100;
-let posY = 100;
+let posX=0;
+let posY=0;
+let spiderSpeed;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  spiderSize = random(30, 100);
+  spiderSize = random(30, 90);
 }
 
 function draw() {
   background(0);
+  frameRate(165);
 
   fill(255, 100, 0);
   circle(posX, posY, spiderSize);
 
   let d = dist(posX, posY, mouseX, mouseY);
-  /////////
-  if (d > 100) {
+  spiderSpeed = d / 60;
 
-    //horizontal movement
-    if (posX < mouseX) {
-      posX += 10;
-    } else {
-      posX -= 10;
-    }
-
-    //vertical movement
-    if (posY < mouseY) {
-      posY += 10;
-    } else {
-      posY -= 10;
-    }
-
+  if (d > spiderSize) {
+    posX += (mouseX - posX) / spiderSize;
+    posY += (mouseY - posY) / spiderSize;
   }
-  //////////
+
+  fill(255);
+  circle(posX+spiderSize/4, posY, spiderSize/4);
+  circle(posX-spiderSize/4, posY, spiderSize/4);
 }
